865f99e9d7bd6d77a413dd8a206b0b2e55735d9f
Midgard <malmeidamdam1@gmail.com>
2023-11-02 14:04:04 +0100

    Primer comentario
    Comienzo de la ventana principal y  el menú de navegación

---
+++ Estilos de Midgard/Ventana3.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Estilos_de_Midgard
+{
+    /// <summary>
+    /// Lógica de interacción para Ventana3.xaml
+    /// </summary>
+    public partial class Ventana3 : Window
+    {
+        public Ventana3()
+        {
+            InitializeComponent();
+        }
+    }
+}
